2606384b3704b772bcd5c2fddd0ccde30ef13bea
anudeike <ikechukwuanude@gmail.com>
2020-09-14 21:27:01 -0500

    got the rapid fire botmometer api working

--- using_botometer_api.py
+++ using_botometer_api.py
@@ -1,13 +1,8 @@
-import pandas as pd
-import json
 import botometer
 from dotenv import load_dotenv
 import os
+import json
 
-# FORMAT OF THE DATA
-# ,id,screen_name,follower_count
-
-# set globals
 load_dotenv()
 
 rapidapi_key = os.getenv('RAPID_FIRE_KEY')
@@ -25,15 +20,9 @@ bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
-# path to the data from the earlier df
-path = "data/basic_user_data.csv"
 
-df = pd.read_csv(path)
-
-print(df)
-def main():
-    ## TO DO: GET THE INFORMATION FROM THE BOTOMETER AND PUT INTO THE LIST
-    pass
-
-main()
-
+accounts = ['@clayadavis', '@onurvarol', '@jabawack']
+for screen_name, result in bom.check_accounts_in(accounts):
+    # Do stuff with `screen_name` and `result`
+    print(screen_name)
+    print(result)
