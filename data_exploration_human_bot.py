231589d3b2a244baa933fa75e3539a8d0eff5302
anudeike <ikechukwuanude@gmail.com>
2020-10-10 04:12:03 -0500

    can oly get around 94% accuracy, which is okay I guess

--- data_exploration_human_bot.py
+++ data_exploration_human_bot.py
@@ -8,6 +8,9 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier # RANDOM FOREST
+from sklearn import svm, tree # using the svm and tree classifers
+import xgboost # special?
 from sklearn import preprocessing
 import pickle
 from sklearn.model_selection import KFold
@@ -15,7 +18,7 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import LeavePOut
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_confusion_matrix, accuracy_score
 
 # the goal of this file
 # is to explore the already gathered data
@@ -38,8 +41,24 @@ def turn_orgs_to_bots(df):
     df[df['labels'] == 2] = 0
     return df
 
+def get_rid_of_orgs(df):
 
+    df = df[df['labels'] != 2]
+    return df
 
+def print_feature_importances(arr):
+
+    feature_names = ["CAP","astroturf","fake_follower","financial","other","overall","self-declared"]
+
+    print(arr)
+    if len(arr) != len(feature_names):
+        print(len(feature_names))
+        print(len(arr))
+        print("the amount of features must be the same length")
+        return
+
+    for i in range(feature_names):
+        print(f'Importance of {feature_names[i]}: {arr[i]}')
 #master = pd.read_csv(master_path)
 #print(master.groupby(['labels']).count())
 #describe_plot_save(master[master["labels"] == 0], "plots/meta-data_17k", name="bots_17k")
@@ -54,9 +73,8 @@ def log_reg_holdout():
     bots_humans = turn_orgs_to_bots(master_df)
 
     # split into array for the features and resp vars
-    x1 = bots_humans.drop('labels', axis=1).values
+    x1 = bots_humans.drop(['labels', 'financial', 'self-declared', 'fake_follower', 'CAP'], axis=1).values
     y1 = bots_humans['labels'].values
-
 
     # train using Logistic Regression
     X_train, X_test, Y_train, Y_test = model_selection.train_test_split(x1, y1, test_size=0.30, random_state=100,stratify=y1)
@@ -297,10 +315,106 @@ def log_reg_shuffle_strat(num_fold = 10):
 
     print("Accuracy: %.2f%%" % (res_kfold.mean() * 100.0))
 
+def holdout_all_classifiers():
+    # sort by type
+    # bots = master_df[master_df['labels'] == 0]
+    # humans = master_df[master_df['labels'] == 1]
+    # organizations = master_df[master_df['labels'] == 2]
+    master_df = pd.read_csv(master_path)
+
+    bots_humans = turn_orgs_to_bots(master_df)
+    #bots_humans = get_rid_of_orgs(master_df)
 
 
-log_reg_holdout_save_model("MainModel")
 
-# load the model and use it
-loaded_model = pickle.load(open("models/FirstRun_LogisticRegression.sav", "rb"))
+    # split into array for the features and resp vars
+    removed = ['labels', 'financial', 'self-declared', 'fake_follower', 'CAP']
+    x1 = bots_humans.drop(['labels', 'id'], axis=1).values
+    # print(bots_humans.drop(['labels', 'id'], axis=1).head(5))
+    # return
+    y1 = bots_humans['labels'].values
+
+    # train using Logistic Regression
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(x1, y1, test_size=0.30, random_state=100,stratify=y1)
+
+    # preprocess the data
+    X_scaled = preprocessing.scale(X_train)
+    X_test_scaled = preprocessing.scale(X_test)
+
+    # TRAINING
+
+    model = xgboost.XGBClassifier()
+
+    optimization_dict = {
+        'max_depth': [2, 4, 6],
+        'n_estimators': [50, 200, 500],
+        'learning_rate': [0.1, 0.01, 1],
+
+    }
+
+    model = model_selection.GridSearchCV(model, optimization_dict,
+                          scoring='accuracy', verbose=1)
+
+    model.fit(X_scaled, Y_train)
+    #model.fit(X_scaled, y1)
+
+    # list all the features and their importances
+    #print_feature_importances(model.feature_importances_)
+    #print(model.feature_importances_)
+    print(model.best_score_)
+    print(model.best_params_)
+
+def holdout_all_classifiers_pruned():
+    # sort by type
+    # bots = master_df[master_df['labels'] == 0]
+    # humans = master_df[master_df['labels'] == 1]
+    # organizations = master_df[master_df['labels'] == 2]
+
+    # this one prunes outlier and then throws them back together
+    master_df = pd.read_csv(master_path)
+
+    bots_humans = turn_orgs_to_bots(master_df)
+    #bots_humans = get_rid_of_orgs(master_df)
+
+
+
+    # split into array for the features and resp vars
+    removed = ['labels', 'financial', 'self-declared', 'fake_follower', 'CAP']
+    x1 = bots_humans.drop(['labels', 'id'], axis=1).values
+    # print(bots_humans.drop(['labels', 'id'], axis=1).head(5))
+    # return
+    y1 = bots_humans['labels'].values
+
+    # train using Logistic Regression
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(x1, y1, test_size=0.30, random_state=100,stratify=y1)
+
+    # preprocess the data
+    X_scaled = preprocessing.scale(X_train)
+    X_test_scaled = preprocessing.scale(X_test)
+
+    # TRAINING
+
+    model = xgboost.XGBClassifier()
+
+    optimization_dict = {
+        'max_depth': [2, 4, 6],
+        'n_estimators': [50, 200, 500],
+        'learning_rate': [0.1, 0.01, 1],
+
+    }
+
+    model = model_selection.GridSearchCV(model, optimization_dict,
+                          scoring='accuracy', verbose=1)
+
+    model.fit(X_scaled, Y_train)
+    #model.fit(X_scaled, y1)
+
+    # list all the features and their importances
+    #print_feature_importances(model.feature_importances_)
+    #print(model.feature_importances_)
+    print(model.best_score_)
+    print(model.best_params_)
+
+
+holdout_all_classifiers()
 
--- models/classify_account.py
+++ models/classify_account.py
@@ -2,5 +2,110 @@
 This file classifies an account as a human or non-human.
 The only input is the account name or id.
 """
+import pickle
+import botometer
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_botometer_stats_single_account(bom_instance, screen_name):
+    """
+    Gets statistics about the account from the botometer API
+    :param bom_instance: An authenticated instance of the botometer object
+    :param screen_name: The screen name of the account
+    :return: dict
+    """
+
+    result = bom_instance.check_account(screen_name)
+
+    try:
+        if (result["user"]["majority_lang"] == 'en'):
+            # use the english results
+
+            # for each row that'll be appended
+            row = {
+                "id": screen_name,
+                "CAP": result['cap']['english'],
+                # "astroturf": result['display_scores']['english']['astroturf'],
+                "fake_follower": result['display_scores']['english']['fake_follower'],
+                "financial": result['display_scores']['english']['financial'],
+                "other": result['display_scores']['english']['other'],
+                "overall": result['display_scores']['english']['overall'],
+                "self-declared": result['display_scores']['english']['self_declared'],
+                # "spammer": result['display_scores']['english']['spammer'],
+            }
+        else:
+
+            row = {
+                "id": screen_name,
+                "CAP": result['cap']['universal'],
+                # "astroturf": result['display_scores']['universal']['astroturf'],
+                "fake_follower": result['display_scores']['universal']['fake_follower'],
+                "financial": result['display_scores']['universal']['financial'],
+                "other": result['display_scores']['universal']['other'],
+                "overall": result['display_scores']['universal']['overall'],
+                "self-declared": result['display_scores']['universal']['self_declared'],
+                # "spammer": result['display_scores']['universal']['spammer'],
+            }
+
+        return row
+
+    except Exception as e:
+        # skip if error
+        print("{} Could not be fetched: {}".format(id, e))
+
+        return None
+
+def load_model(model_path = "MainModel_LogisticRegression.sav"):
+    """
+    Returns an instance of the model. The model must be created using the pickle library
+    :param model_path: Path of the file that contains the model
+    :return: the loaded model
+    """
+    return pickle.load(open(model_path, 'rb'))
+
+def process_single_account(model, stats):
+
+    stats = list(stats.values())
+
+    prediction = model.predict([stats[1:]])
+
+    if prediction == 0:
+        print(f'We classify {stats[0]} as a non-human user.')
+
+    if prediction == 1:
+        print(f'We classify {stats[0]} as a human user.')
+
+    pass
+
+
+
+if __name__ == "__main__":
+
+    rapidapi_key = os.getenv('RAPID_FIRE_KEY')
+
+    # authentication
+    twitter_app_auth = {
+        'consumer_key': os.getenv('TWITTER_API_KEY'),
+        'consumer_secret': os.getenv('TWITTER_API_SECRET'),
+        'access_token': os.getenv('TWITTER_ACCESS_TOKEN'),
+        'access_token_secret': os.getenv('TWITTER_ACCESS_SECRET'),
+    }
+
+    #print(twitter_app_auth)
+    # set up botometer - auth
+    bom = botometer.Botometer(wait_on_ratelimit=True,
+                              rapidapi_key=rapidapi_key,
+                              **twitter_app_auth)
+
+    # TODO get id/name from the user
+    # TODO decide whether the input is a whole stream of information of just one
+
+    screen_name = "@clayadavis"
+
+    res = get_botometer_stats_single_account(bom, screen_name)
+    model = load_model()
+    process_single_account(model=model, stats=res)
 
 
