440731563471625c01b69a37ecc2656bd9f6af75
anudeike <ikechukwuanude@gmail.com>
2021-01-07 17:13:11 -0600

    should be able to process all of the tweets now

--- aspect_based_learning/main.py
+++ aspect_based_learning/main.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import json
 import string
-from nltk.corpus import stopwords
+import re
 
-stop_words_en = set(stopwords.words('english'))
 
 # print(stop_words_en)
 # exit(0)
 path = "vaccine-2020-07-09.txt"
 
-def tweet_to_json():
+def tweet_to_json(limit=3):
 
     tweet = {}
 
     with open(path, "r") as f:
-        f.readline()
-        f.readline()
+        for x in range(0, limit):
+            f.readline()
+
         line = f.readline()
 
         tweet = json.loads(line)
@@ -23,11 +23,79 @@ def tweet_to_json():
     with open("test.json", "w") as j:
         json.dump(tweet, j)
 
+def get_full_tweet_text(tweet_data: dict):
 
-def get_tweets():
+    # check if truncated
+    if tweet_data["truncated"]:
+        # get the extended tweet
+        return tweet_data["extended_tweet"]["full_text"]
+    else:
+        # if not retweeted
+        if "retweeted_status" not in tweet_data:
+            return tweet_data["text"]
+
+        # if retweet
+        if tweet_data["retweeted_status"]:
+            # and you're truncated
+            if tweet_data["retweeted_status"]["truncated"]:
+                return tweet_data["retweeted_status"]["extended_tweet"]["full_text"]
+
+            return tweet_data["text"]
+
+
+def preprocess_tweet(full_text):
+    """
+    Pre-Processes the tweet text to be used.
+    1. Convert to Lowercase
+    2. Remove URL
+    3. remove retweets and cc
+    4. Remove Hashtags
+    5. Remove Emojis
+    6. Remove html tags
+    7. Remove extra spaces
+    8. Remove Punctuation
+    :param full_text: full tweet text
+    :return: processed text
+    """
+
+    # convert to lower case
+    full_text = full_text.lower()
+
+    # url removes
+    full_text = re.sub(r'(https|http)?:\/(\w|\.|\/|\?|\=|\&|\%)*\b', '', full_text)
+    full_text = re.sub(r'www\.\S+\.com', '', full_text)
+
+    # removes retweets & cc
+    full_text = re.sub(r'rt|cc', '', full_text)
+
+    # hashtags removes
+    full_text = re.sub(r'#\S+', '', full_text)
+
+    # user mention removes
+    full_text = re.sub(r'@\S+', '', full_text)
+
+    # emoji
+    full_text = re.sub(r'[^\x00-\x7F]+', '', full_text)
+
+    # html tags
+    full_text = re.sub(r'<.*?>', '', full_text)
+
+    # removes extra spaces
+    full_text = re.sub(r' +', ' ', full_text)
+    full_text = re.sub(r'/(\r\n)+|\r+|\n+|\t+/', " ", full_text)
+
+    # leave punctuation out the model should be able to handle it
+    # # punctuation
+    # full_text = full_text.re.sub('[{}]'.format(string.punctuation), '')
+
+    #print(full_text)
+
+    return full_text
+
+def get_tweets(limit=3):
 
     # create a dataframe
-    df = pd.DataFrame(columns=['tweets'])
+    #df = pd.DataFrame(columns=['tweets'])
 
 
     # goal
@@ -45,12 +113,24 @@ def get_tweets():
                 # deserialize
                 tweet_data = json.loads(line)
 
-                tweet_content = tweet_data['text']
+                # get the full text of the tweet
+                try:
+                    full_tweet_text = get_full_tweet_text(tweet_data)
 
-                # insert info
-                df.loc[index] = [tweet_content]
+                    tweet_content = preprocess_tweet(full_tweet_text)
 
-                if index > 4:
+                    print(f'{index}: TWEET TEXT: {tweet_content}')
+
+                    # insert info
+                    # df.loc[index] = [tweet_content]
+                except Exception as e:
+                    # in final code: will change to skipping this iteration and going to the next one.
+                    print(f'ERROR: {e}')
+                    continue # skip
+
+
+
+                if index > limit:
                     break
 
                 index += 1
@@ -65,60 +145,21 @@ def get_tweets():
         # send the info to a csv file
         #print(df)
 
-    return df
+    #return df
 
-def filter_stop_words(x):
-    print(x)
-    return x
 
-def preprocess_tweets(data):
 
-    # convert to lower case
-    data['preprocess_data'] = data['tweets'].str.lower()
-
-    # url removes
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'(https|http)?:\/(\w|\.|\/|\?|\=|\&|\%)*\b', '')
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'www\.\S+\.com', '')
-    #
-    # removes retweets & cc
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'rt|cc', '')
-    #
-    # hashtags removes
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'#\S+', '')
-
-    # user mention removes
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'@\S+', '')
-    #
-    # emoji
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'[^\x00-\x7F]+', '')
-    #
-    # html tags
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r'<.*?>', '')
-    #
-    # removes extra spaces
-    data['preprocess_data'] = data['preprocess_data'].str.replace(r' +', ' ')
-    #
-    # punctuation
-    data['preprocess_data'] = data['preprocess_data'].str.replace('[{}]'.format(string.punctuation), '')
-    #
-    # stop words removes
-    data['preprocess_data'] = data['preprocess_data'].apply(lambda x: filter_stop_words(x))
-    #
-    # # convert preprocessed list words to string
-    # data['preprocess_str'] = data['preprocess_data'].apply(' '.join)
-
-    return data
 
 def main():
-    t = get_tweets()
-    pp = preprocess_tweets(t)
+    t = get_tweets(limit=10)
+    #pp = preprocess_tweets(t)
 
-    #tweet_to_json()
+    #tweet_to_json(limit=4)
 
-    print(pp)
+    #print(pp)
 
     # write to a csv
-    pp.to_csv('test.csv', index=False)
+    #pp.to_csv('test.csv', index=False)
 
     pass
 
